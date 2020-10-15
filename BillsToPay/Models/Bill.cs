41f3e3bf677900bdf75f2d89e8393016bafcb52b
Rafael Ahrons <rafael.ahrons@gmail.com>
2020-10-15 09:49:13 -0300

    gitignore

---
+++ BillsToPay/Models/Bill.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BillsToPay.Models
+{    
+    public class Bill
+    {        
+
+    }
+}
---
+++ BillsToPay/Services/BillsToPayService.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using BillsToPay.Models;
+
+namespace BillsToPay.Services
+{
+    public class BillsToPayService
+    {
+        public List<Bill> GetBills()
+        {
+            return new List<Bill>();
+        }
+    }
+}
