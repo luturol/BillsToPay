d634e56eed296d8b9fcb4181c843d298a8e18707
Rafael Ahrons <rafael.ahrons@gmail.com>
2020-10-28 14:28:25 -0300

    Add docker-compose

--- BillsToPay/Startup.cs
+++ BillsToPay/Startup.cs
@@ -30,7 +30,7 @@ namespace BillsToPay
         {
             services.AddTransient<IBillsToPayRepository, BillsToPayRepository>();
             services.AddTransient<IBillsToPayService, BillsToPayService>();
-            services.AddControllers();
+            services.AddControllers();            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +41,7 @@ namespace BillsToPay
                 app.UseDeveloperExceptionPage();
             }
 
+            
             app.UseHttpsRedirection();
 
             app.UseRouting();
